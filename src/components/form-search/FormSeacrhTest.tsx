6a96a10d5f9574a7248e5f2105de0a7b129cf354
Dmitriy <aegis.ko20@mail.ru>
2023-09-04 20:09:14 +0300

    dfd

--- src/components/form-search/FormSeacrhTest.tsx
+++ src/components/form-search/FormSeacrhTest.tsx
@@ -1,90 +1,154 @@
 import cn from 'clsx';
 import React, { FC, useState } from 'react';
 import { useForm } from 'react-hook-form';
+import { $axios } from '../../api';
 import styles from './FormSeacrh.module.scss';
 
 const FormSeacrhTest: FC = () => {
 	const [colorDateStart, setColorDateStart] = useState(0);
 	const [colorDateEnd, setColorDateEnd] = useState(0);
 
-	const [isChecked, setIsChecked] = useState({
-		one: false,
-		two: false,
-		three: false,
-	});
+	const [valueInn, setValueInn] = useState(7710137066);
+	const [value, setValue] = useState(5);
+	const [valueStart, setValueStart] = useState('');
+	const [valueEnd, setValueEnd] = useState('');
+	const [valueSelect, setValueSelect] = useState('');
 
-	const checkedCheckbox = (idChecbox: string) => {
-		const elem = document.getElementById(idChecbox);
-		if (elem.value === 'on') {
-			elem.checked = true;
-		}
-	};
+	// const [isChecked, setIsChecked] = useState({
+	// 	one: false,
+	// 	two: false,
+	// 	// three: false,
+	// });
 
-	// test
-	// const onSubmitSearch = data => {
-	// 	try {
-	// 		const responce = $axios.post('/v1/objectsearch/histograms', {
-	// 			issueDateInterval: {
-	// 				startDate: '',
-	// 				endDate: '',
-	// 			},
-	// 			searchContext: {
-	// 				targetSearchEntitiesContext: {
-	// 					targetSearchEntities: [
-	// 						{
-	// 							inn: 7710137066,
-	// 						},
-	// 					],
-	// 					tonality: '',
-	// 				},
-	// 			},
-	// 			intervalType: 'month',
-	// 			limit: 1000,
-	// 		});
-	// 		console.log(responce);
-	// 		console.log('data:', data);
-	// 	} catch (error) {
-	// 		console.log(error);
+	// const checkedCheckbox = (idChecbox: string) => {
+	// 	const elem = document.getElementById(idChecbox);
+	// 	if (elem.value === 'on') {
+	// 		elem.checked = true;
 	// 	}
 	// };
+
+	// test
+	const onSubmitSearch = data => {
+		setValue(Number(value));
+		setValueInn(Number(valueInn));
+		console.log(typeof valueInn);
+		try {
+			const responce = $axios.post(
+				'/v1/objectsearch/histograms',
+				JSON.stringify({
+					issueDateInterval: {
+						startDate: valueStart,
+						endDate: valueEnd,
+					},
+					searchContext: {
+						targetSearchEntitiesContext: {
+							targetSearchEntities: [
+								{
+									type: 'company',
+									sparkId: null,
+									entityId: null,
+									inn: valueInn,
+									inBusinessNews: null,
+								},
+							],
+							tonality: 'any',
+						},
+					},
+
+					limit: value,
+				})
+			);
+			console.log(responce);
+			console.log('data:', data);
+		} catch (error) {
+			console.log(error);
+		}
+	};
 	// test
 
-	const { register, handleSubmit } = useForm({
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm({
 		// defaultValues: {
-		// 	firstName: '',
-		// 	lastName: '',
-		// 	category: '',
-		// 	checkbox: ``,
-		// 	checkbox1: ``,
-		// 	checkbox2: ``,
+		// 	issueDateInterval: {
+		// 		startDate: '',
+		// 		endDate: '',
+		// 	},
+		// 	searchContext: {
+		// 		targetSearchEntitiesContext: {
+		// 			targetSearchEntities: [
+		// 				{
+		// 					type: 'company',
+		// 					sparkId: null,
+		// 					entityId: null,
+		// 					inn: 7710137066,
+		// 					inBusinessNews: null,
+		// 				},
+		// 			],
+		// 			tonality: 'any',
+		// 		},
+		// 	},
+
+		// 	limit: 1000,
+
+		// 	// checkbox: false,
+		// 	// checkbox1: false,
 		// },
-		defaultValues: {
-			issueDateInterval: {
-				startDate: '',
-				endDate: '',
-			},
-			inn: 7710137066,
-			tonality: '',
-			limit: 1000,
-			intervalType: 'month',
-		},
+		mode: 'onChange',
 	});
+
+	//
+	// {
+	//   "issueDateInterval": {
+	//     "startDate": "2019-01-01T00:00:00+03:00",
+	//     "endDate": "2022-08-31T23:59:59+03:00"
+	//   },
+	//   "searchContext": {
+	//     "targetSearchEntitiesContext": {
+	//       "targetSearchEntities": [
+	//         {
+	//           "inn": 7710137066,
+	//         }
+	//       ],
+	//       "tonality": "any",
+	//     },
+	//   },
+	//   "limit": 1000,
+	// }
+	//
 
 	return (
 		<form
 			className={cn(styles['search__form'], styles.form)}
-			onSubmit={handleSubmit(console.log)}
+			onSubmit={handleSubmit(onSubmitSearch)}
 		>
 			<div className={styles['form__block-input']}>
-				{/* <Input id={'INN'} placeholder='10 цифр'>
-					ИНН компании *
-				</Input> */}
 				<input
-					{...register('inn')}
+					{...register(
+						'searchContext.targetSearchEntitiesContext.targetSearchEntities[0].inn',
+						{
+							required: 'no',
+							pattern: {
+								value: /^[\d+]{10}$/,
+								message: 'Please',
+							},
+						}
+					)}
 					type='text'
 					placeholder='10 цифр'
-					value={7710137066}
+					style={{
+						backgroundColor: 'rgba(70, 90, 126, 0)',
+					}}
+					// value={valueInn}
+					// onChange={event => setValueInn(event?.target.value)}
 				/>
+				{/* {errors.inn && (
+					<div style={{ color: 'red', fontSize: '10px' }}>
+						{errors.inn.message}
+					</div>
+				)} */}
 				<div className={styles['form__block-select']}>
 					<label className={styles['form__label-select']} htmlFor='select'>
 						Тональность
@@ -92,19 +156,25 @@ const FormSeacrhTest: FC = () => {
 					<select
 						defaultValue='any'
 						id='select'
-						{...register(`tonality`, {
+						{...register(`searchContext.targetSearchEntitiesContext.tonality`, {
 							required: 'Введите корректные данные',
 						})}
+						// value={valueSelect}
+						// onChange={event => setValueSelect(event?.target.value)}
 					>
 						<option value='positive'>Позитивная</option>
 						<option value='negative'>Негативная</option>
 						<option value='any'>Любая</option>
 					</select>
 				</div>
-				{/* <Input id={'docs'} placeholder='от 1 до 1000'>
-					Количество документов в выдаче *
-				</Input> */}
-				<input {...register('limit')} type='text' placeholder='от 1 до 1000' />
+
+				<input
+					{...register('limit')}
+					type='text'
+					placeholder='от 1 до 1000'
+					// value={value}
+					// onChange={event => setValue(event?.target.value)}
+				/>
 				<div className={styles['form__block-data-input']}>
 					<label>
 						Диапазон поиска *
@@ -121,6 +191,8 @@ const FormSeacrhTest: FC = () => {
 							{...register(`issueDateInterval.startDate`, {
 								required: 'Введите корректные данные',
 							})}
+							value={valueStart}
+							onChange={event => setValueStart(event?.target.value)}
 						/>
 					</label>
 					<label>
@@ -135,49 +207,44 @@ const FormSeacrhTest: FC = () => {
 							style={{
 								color: `rgba(0, 0, 0, ${colorDateEnd})`,
 							}}
-							{...register(`issueDateInterval`, {
+							{...register(`issueDateInterval.endDate`, {
 								required: 'Введите корректные данные',
 							})}
+							value={valueEnd}
+							onChange={event => setValueEnd(event?.target.value)}
 						/>
 					</label>
 				</div>
 			</div>
 			<div className={styles['form__block-checkbox']}>
 				<div>
-					{/* <Checkbox id={'max'}>Признак максимальной полноты</Checkbox> */}
-					{/* <Checkbox id={'context'}>Упоминания в бизнес-контексте</Checkbox>
-					<Checkbox id={'public'}>Главная роль в публикации</Checkbox>
-					<Checkbox id={'factors'}>Публикации только с риск-факторами</Checkbox>
-					<Checkbox id={'news'}>Включать технические новости рынков</Checkbox>
-					<Checkbox id={'calendar'}>Включать анонсы и календари</Checkbox>
-					<Checkbox id={'onNews'}>Включать сводки новостей</Checkbox> */}
 					{/* <input
 						{...register('checkbox')}
 						id='1'
 						type='checkbox'
-						onClick={() => {
-							if (isChecked) {
-								setIsChecked({ ...isChecked, one: false });
-							} else {
-								setIsChecked({ ...isChecked, one: true });
-								checkedCheckbox('1');
-							}
-						}}
+						// onClick={() => {
+						// 	if (isChecked) {
+						// 		setIsChecked({ ...isChecked, one: false });
+						// 	} else {
+						// 		setIsChecked({ ...isChecked, one: true });
+						// 		checkedCheckbox('1');
+						// 	}
+						// }}
 					/>
 					<input
 						{...register('checkbox1')}
 						id='2'
 						type='checkbox'
-						onClick={() => {
-							if (isChecked) {
-								setIsChecked({ ...isChecked, one: false });
-							} else {
-								setIsChecked({ ...isChecked, one: true });
-								checkedCheckbox('2');
-							}
-						}}
-					/>
-					<input
+						// onClick={() => {
+						// 	if (isChecked) {
+						// 		setIsChecked({ ...isChecked, one: false });
+						// 	} else {
+						// 		setIsChecked({ ...isChecked, one: true });
+						// 		checkedCheckbox('2');
+						// 	}
+						// }}
+					/> */}
+					{/* <input
 						{...register('checkbox2')}
 						id='3'
 						type='checkbox'
